67e9c07e829fc5a96704f8cf6617e0dfc57a240e
“pindoortega” <andresortegapindo@gmail.com>
2023-03-11 23:43:19 -0500

    guarda otra vez 1234

--- wp-content/themes/gymfitness/front-page.php
+++ wp-content/themes/gymfitness/front-page.php
@@ -7,7 +7,7 @@
         <p><?php the_field('texto_bienvenida') ?></p>
     </section>
 
-    <section class="area"> hola este es el cambio de 
+    <section class="area">
 
         <div class="area">
             <?php
