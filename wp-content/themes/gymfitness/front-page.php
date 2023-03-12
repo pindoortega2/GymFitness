a818f99414fcb3cc046be221b8c483d509d8967e
“pindoortega” <andresortegapindo@gmail.com>
2023-03-11 23:37:25 -0500

    guarda otra

---
+++ wp-content/themes/gymfitness/front-page.php
@@ -0,0 +1,60 @@
+<?php get_header() ?>      
+
+    <section class="bienvenida seccion contenedor text-center">
+        <h2 class="text-primary">
+            <?php the_field('encabezado_bienvenida') ?>
+        </h2>
+        <p><?php the_field('texto_bienvenida') ?></p>
+    </section>
+
+    <section class="area">
+
+        <div class="area">
+            <?php
+                $area1 = get_field('area_1'); 
+            ?>
+
+            <img src="<?php echo $area1['imagen']['sizes']['medium_large'] ?> ">
+            <p><?php echo $area1['texto']?></p>
+            
+        </div>
+
+        <div class="area">
+            <?php
+                $area2 = get_field('area_2'); 
+            ?>
+            
+            <img src="<?php echo $area2['imagen']['sizes']['medium_large'] ?> ">
+            <p><?php echo $area2['texto']?></p>
+            
+        </div>
+
+        <div class="area">
+            <?php
+                $area3 = get_field('area_3'); 
+            ?>
+            
+            <img src="<?php echo $area3['imagen']['sizes']['medium_large'] ?> ">
+            <p><?php echo $area3['texto']?></p>
+            
+        </div>
+
+        <div class="area">
+            <?php
+                $area4 = get_field('area_4'); 
+            ?>
+            
+            <img src="<?php echo $area4['imagen']['sizes']['medium_large'] ?> ">
+            <p><?php echo $area4['texto']?></p>
+            
+        </div>
+
+    </section>
+
+    <main class="contenedor seccion">
+            
+    </main>
+
+
+<?php get_footer() ?>
+
